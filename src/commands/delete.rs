940ce225c7e35cdafab66a861eecf0848804ba29
Sam GUEST <sam.r@argodevops.co.uk>
2023-03-09 12:54:34 +0000

    feat(api): Made delete more inituative

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -2,9 +2,9 @@ use clap::Args;
 
 #[derive(Debug, Args)]
 pub struct Delete {
-    /// The application ids to delete
-    #[clap(short, long, conflicts_with("all"))]
-    pub ids: Option<Vec<String>>,
+    /// The application ids to delete, space seperated to delete multiple applications at once
+    #[clap(index = 1, required_unless_present = "all", conflicts_with("all"))]
+    pub ids: Vec<String>,
     /// If the all flag is set all composer applications will be deleted
     #[clap(long)]
     pub all: bool,
