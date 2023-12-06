a44823c15c766e3af7e9843b9798f88ceaded27f
Sam Ruff <sam@technesci.co.uk>
2023-12-06 22:43:28 +0000

    feat: Updated to use semver and fixing versions built on different environments

---
+++ release.config.js
@@ -0,0 +1,29 @@
+const EXTERNAL_PIP_INDEX_URL = process.env.EXTERNAL_PIP_INDEX_URL;
+
+const branches = [
+    'main',
+    {
+        name: 'alpha',
+        prerelease: true
+    },
+    {
+        name: 'fix-build',
+        prerelease: "rc"
+    }
+];
+
+const commitAnalyzer = '@semantic-release/commit-analyzer';
+
+const git = '@semantic-release/git';
+
+const exec = [
+    "@semantic-release/exec",
+    {
+        "prepareCmd": "echo \"RELEASE_VERSION=${nextRelease.version}\" >> $GITHUB_ENV"
+    }
+];
+
+module.exports = {
+    branches,
+    plugins: [commitAnalyzer, git, exec]
+};
